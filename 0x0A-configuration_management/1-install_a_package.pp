9ed55c1d14b5221012ea5d6050b67a97f2162733
ifiok1equere <equereifiok1@gmail.com>
2023-12-22 22:28:25 +0100

    make package more robust

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
-# this code defines a package resource
+# define the package resource
+
+# Install Python 3.8.10 using the package resource
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+
+# Install Flask and Werkzeug using pip3
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
