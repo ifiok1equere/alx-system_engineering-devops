84c4b26c1cfecc1c79bd54795d2a9c90506bb961
ifiok1equere <equereifiok1@gmail.com>
2023-12-22 15:43:19 +0100

    task 0: 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This declares a file resource.
+
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love puppet',
+}
