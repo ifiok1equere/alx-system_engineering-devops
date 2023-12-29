9f823e7b556f0b0be163e5b85498ef11c4bdf23d
ifiok1equere <equereifiok1@gmail.com>
2023-12-30 00:16:26 +0100

    advanced task 4: Client configuration file (w/Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,26 @@
-# config ssh with puppet
+# configure ssh with puppet
 
+# Declare a class for managing SSH configuration
 class ssh_config {
+  # Ensure the SSH config file exists
   file { '/home/iwequer/.ssh/config':
-    ensure  => present,
-    content => "Host  *\n    HostName 34.227.94.114\n    User ubuntu\n    IdentityFile ~/.ssh/school\n    IdentitiesOnly yes\n    PasswordAuthentication no\n",
+    ensure => present,
+  }
+
+  # Manage a specific line in the SSH config file to turn off password authentication
+  file_line { 'Turn off passwd auth':
+    path  => '/home/iwequer/.ssh/config',
+    line  => 'PasswordAuthentication no',
+    match => '^#?PasswordAuthentication',
+  }
+
+  # Manage another line in the SSH config file to declare the identity file
+  file_line { 'Declare identity file':
+    path  => '/home/iwequer/.ssh/config',
+    line  => 'IdentityFile ~/.ssh/school',
+    match => '^#?IdentityFile',
   }
 }
 
+# Include the ssh_config class to apply the configuration
 include ssh_config
