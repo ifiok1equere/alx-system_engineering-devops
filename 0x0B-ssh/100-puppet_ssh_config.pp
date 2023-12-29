8fa6612f1393dc908710df8adb9e2ff53543e5c4
ifiok1equere <equereifiok1@gmail.com>
2023-12-30 00:11:57 +0100

    advanced task 4: Client configuration file (w/Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
-# use puppet to make changes to ssh config file
+# config ssh with puppet
 
 class ssh_config {
   file { '/home/iwequer/.ssh/config':
     ensure  => present,
-    content => "Host  *\n    HostName 34.227.94.114\n    User ubuntu\n    IdentityFile ~/.ssh/school\n    IdentitiesOnly yes\n",
+    content => "Host  *\n    HostName 34.227.94.114\n    User ubuntu\n    IdentityFile ~/.ssh/school\n    IdentitiesOnly yes\n    PasswordAuthentication no\n",
   }
 }
 
