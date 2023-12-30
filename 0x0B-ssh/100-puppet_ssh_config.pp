bc3b22b581b93b5b482023bb40ab108c177e15a8
ifiok1equere <equereifiok1@gmail.com>
2023-12-30 16:48:00 +0100

    resolve checker

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,26 +1,11 @@
-# configure ssh with puppet
-
-# Declare a class for managing SSH configuration
-class ssh_config {
-  # Ensure the SSH config file exists
-  file { '/home/iwequer/.ssh/config':
-    ensure => present,
-  }
-
-  # Manage a specific line in the SSH config file to turn off password authentication
-  file_line { 'Turn off passwd auth':
-    path  => '/home/iwequer/.ssh/config',
-    line  => 'PasswordAuthentication no',
-    match => '^#?PasswordAuthentication',
-  }
-
-  # Manage another line in the SSH config file to declare the identity file
-  file_line { 'Declare identity file':
-    path  => '/home/iwequer/.ssh/config',
-    line  => 'IdentityFile ~/.ssh/school',
-    match => '^#?IdentityFile',
-  }
+file_line { 'Turn off passwd auth':
+    path   => '/home/iwequer/.ssh/config',
+    line   => '    PasswordAuthentication no',
+    match  => '^.*PasswordAuthentication.*',
 }
 
-# Include the ssh_config class to apply the configuration
-include ssh_config
+file_line { 'Declare identity file':
+    path   => '/home/iwequer/.ssh/config',
+    line   => '    IdentityFile ~/.ssh/school',
+    match  => '^.*IdentityFile.*',
+}
